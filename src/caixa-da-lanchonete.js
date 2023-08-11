316aecef8c71e70210701e699349919b0b4e7eed
Gustavo Costa <contato.gustavopimentel@gmail.com>
2023-08-11 10:45:00 -0300

    feat: import constants and define error enum

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,34 @@
+import CARDAPIO from './cardapio';
+import TAXAS from './taxas';
+import DESCONTOS from './descontos';
+import FORMAS_DE_PAGAMENTO from './formas-de-pagamento';
+
+const MENSAGENS_DE_ERRO = Object.freeze({
+	ITEM_PRINCIPAL_INEXISTENTE: 'Item extra não pode ser pedido sem o principal',
+	CARRINHO_VAZIO: 'Não há itens no carrinho de compra!',
+	ZERO_ITENS: 'Quantidade inválida!',
+	CODIGO_INEXISTENTE: 'Item inválido!',
+	FORMAS_DE_PAGAMENTO_INEXISTENTE: 'Forma de pagamento inválida!',
+});
+
 class CaixaDaLanchonete {
+	calcularValorDaCompra(metodoDePagamento, itens) {
+		const metodoPagamento = FORMAS_DE_PAGAMENTO[metodoDePagamento];
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
-    }
-
+		if (metodoDePagamento === null) {
+			return;
+		}
+		// calcularValorDaCompra(string[debito, credito, dinheiro], itens[array[string[cod,qnt]]])
+	}
 }
 
 export { CaixaDaLanchonete };
+
+// Atualmente a Lanchonete aceita as seguintes formas de pagamento:
+
+// dinheiro
+// debito
+// credito
+// O sistema deve receber essa informação como string, utilizando a grafia exatamente igual aos exemplos acima.
+
+//  Você pode desenvolver a sua lógica criando outros arquivos, métodos e até mesmo outras classes, porém o resultado deve poder ser obtido através do método calcularValorDaCompra.
