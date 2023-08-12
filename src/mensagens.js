1905e3be2d49083a811478fae1a0a7fde67ee807
Gustavo Costa <contato.gustavopimentel@gmail.com>
2023-08-12 13:40:18 -0300

    feat: add constants to error messages values

---
+++ src/mensagens.js
@@ -0,0 +1,9 @@
+const MENSAGENS_DE_ERRO = Object.freeze({
+	ITEM_PRINCIPAL_INEXISTENTE: 'Item extra não pode ser pedido sem o principal',
+	CARRINHO_VAZIO: 'Não há itens no carrinho de compra!',
+	ZERO_ITENS: 'Quantidade inválida!',
+	CODIGO_INEXISTENTE: 'Item inválido!',
+	FORMAS_DE_PAGAMENTO_INEXISTENTE: 'Forma de pagamento inválida!',
+});
+
+export default MENSAGENS_DE_ERRO;
